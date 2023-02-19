c82b25f1f4fbbd03f1dcf86967f620331f328e74
byte <118949529+blubware@users.noreply.github.com>
2023-02-19 04:39:31 -0800

    Add files via upload

--- README.md
+++ README.md
@@ -6,14 +6,59 @@ Run 'start.bat'
 ```
 Now you should be setup for anti token-logging!
 
+# Configuring
+```
+{
+    "token": "your-token-here",
+    "delay": 30,
+
+    "password_changing": true,
+    "password": "",
+    "another_password": "",
+    "email": "",
+
+    "webhook_logging": true,
+    "webhook_url": ""
+}
+```
+```
+- 'token' 
+-- Your discord token
+
+- 'delay'
+-- Delay at which antilogger checks for new sessions
+
+- 'password_changing'
+-- Changes password on an unauthorized user logging in
+
+    - 'password'
+    -- If 'password_changing' is set to true, fill this out with your current discord password
+
+    - 'another_password'
+    -- If 'password_changing' is set to true, fill this out with another password
+
+    - 'email'
+    -- If 'password_changing' is set to true, fill this out with your current discord email
+
+- 'webhook_logging'
+-- Sends a message with information about any unauthroized user
+
+    - 'webhook_url'
+    -- The webhook url to use with webhook logging
+```
 # Channel-log
 ```
+v0.0.2
+- Password change on detection
+- Webhook notifications
+- Send new discord token to webhook
+
 v0.0.1
 - Basic Functionality
 ```
 # To-do
 ```
-Password change on detection
-Webhook notifications
 Better looking console
+Send all potential attacker information available
+Panic button for account deletion
 ```
--- anti_logger/anti_logger.py
+++ anti_logger/anti_logger.py
@@ -1,4 +1,5 @@
 import requests, json, time, ctypes
+from datetime import datetime
 from colorama import Fore
 
 with open('config.json') as config:
@@ -6,6 +7,18 @@ with open('config.json') as config:
 
     token = config.get('token')
     delay = config.get('delay')
+
+    password_changing = config.get('password_changing')
+    
+    if password_changing:
+        password = config.get('password')
+        another_password = config.get('another_password')
+        email = config.get('email')
+
+    webhook_logging = config.get('webhook_logging')
+
+    if webhook_logging:
+        webhook_url = config.get('webhook_url')
 
 banner = r'''
    ▄▄▄·  ▐ ▄ ▄▄▄▄▄▪  ▄▄▌         ▄▄ •  ▄▄ • ▄▄▄ .▄▄▄  
@@ -41,6 +54,27 @@ def anti_logger():
             session = x.get('id_hash')
             if session not in authorized_sessions:
                 print(f'┃  {Fore.RED}[ALERT] {Fore.RESET}Unknown user has logged into your account')
+                if password_changing:
+                    change_url = f'https://discord.com/api/v9/users/@me'
+                    payload = {'password': password,'new_password': another_password}
+                    change_password = requests.patch(change_url,headers={'authorization': token}, json=payload)
+                    print(f'┃  {Fore.RED}[ALERT] {Fore.RESET}Password has been changed to {another_password}')
+                    
+                    with open('logs.txt', 'a') as logfile:
+                        logfile.write(f'PASSWORD CHANGED FROM {password} TO {another_password} AT {datetime.now()}\n')
+
+                    request_3 = requests.post('https://discord.com/api/v9/auth/login', headers={'email': email, 'password': another_password})
+                    response = json.loads(request_3.text)
+                    new_token = response.get('token')
+
+                if webhook_logging and password_changing:
+                    payload = {"embeds": [{"color": '16711680', "fields": [{"name": "Password Changed To", "value": another_password}][{"name": "Token Changed To", "value": new_token}], "footer": {"text": datetime.now()}}],"username": "antilogger notifications", "avatar_url": "https://i.pinimg.com/564x/9d/0a/f1/9d0af17f210624e75c0a57d4f6ae9427.jpg"}
+                    requests.post(webhook_url, json=payload)
+
+                if webhook_logging and not password_changing:
+                    payload = {"embeds": [{"color": 16711680,"fields": [{"name": "ALERT", "value": f"Unknown user logged into your account at {datetime.now()}"}],"footer": {"text": "sent from antilogger"}}], "username": "antilogger notifications", "avatar_url": "https://i.pinimg.com/564x/9d/0a/f1/9d0af17f210624e75c0a57d4f6ae9427.jpg"}
+                    requests.post(webhook_url, json=payload)
+
 
         temp_delay = delay
         for x in range(delay):
